025a3c38ba1d08aa4bf9f311a657614447d99607
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-27 00:03:20 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search(string)
+  def self.search(string)
     if string == ""
       return Student.all
     else
