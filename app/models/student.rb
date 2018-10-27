64d2ac5e75c0196084be5d72bfd76f97fbe852c2
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-27 00:06:51 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -21,7 +21,7 @@ class Student < ActiveRecord::Base
       array = []
       all = Student.all
       all.each do |student|
-        if student.name.include?(string)
+        if student.name.lowercase.include?(string)
           array << string
         end
       end
